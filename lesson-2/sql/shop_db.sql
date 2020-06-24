5478e18e62d1dbe2de2f47dfda02a12b32ccc678
ollie <o.mialkivska@gmail.com>
2020-06-24 21:31:11 +0300

    lesson-1 db improved

--- lesson-2/sql/shop_db.sql
+++ lesson-2/sql/shop_db.sql
@@ -27,21 +27,13 @@ CREATE TABLE IF NOT EXISTS `hillel_shop`.`category` (
 ) ENGINE = InnoDB;
 
 --
--- Create table `product_price`
---
-CREATE TABLE IF NOT EXISTS `hillel_shop`.`product_price` (
-    `product_id` INT UNSIGNED NOT NULL,
-    `price` DECIMAL UNSIGNED NOT NULL,
-    PRIMARY KEY (`product_id`)
-) ENGINE = InnoDB;
-
---
 -- Create table `product`
 --
 CREATE TABLE IF NOT EXISTS `hillel_shop`.`product` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(255) NOT NULL,
     `description` LONGTEXT NULL,
+    `price` FLOAT UNSIGNED NOT NULL,
     `vendor_id` INT UNSIGNED NOT NULL,
     `category_id` INT UNSIGNED NOT NULL,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -58,12 +50,7 @@ CREATE TABLE IF NOT EXISTS `hillel_shop`.`product` (
         FOREIGN KEY (`category_id`)
             REFERENCES `hillel_shop`.`category` (`id`)
             ON DELETE RESTRICT
-            ON UPDATE RESTRICT,
-    CONSTRAINT `fk_product_price`
-        FOREIGN KEY (`id`)
-            REFERENCES `hillel_shop`.`product_price` (`product_id`)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE
+            ON UPDATE RESTRICT
 ) ENGINE = InnoDB;
 
 --
@@ -73,6 +60,10 @@ CREATE TABLE IF NOT EXISTS `hillel_shop`.`user` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `login` VARCHAR(100) NOT NULL,
     `password` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `status` ENUM('ON', 'OFF') NOT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted_at` DATETIME NULL,
     PRIMARY KEY (`id`),
     INDEX `idx_login_psw` (`login`, `password`)
 ) ENGINE = InnoDB;
@@ -83,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `hillel_shop`.`user` (
 CREATE TABLE IF NOT EXISTS `hillel_shop`.`order` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `user_id` INT UNSIGNED NOT NULL,
-    `order_price` DOUBLE NOT NULL,
+    `order_price` FLOAT NOT NULL,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `deleted_at` DATETIME NULL,
     PRIMARY KEY (`id`),
@@ -105,9 +96,6 @@ CREATE TABLE IF NOT EXISTS `hillel_shop`.`user_profile` (
     `email` VARCHAR(100) NULL,
     `phone` BIGINT NULL,
     `birthday` DATE NULL,
-    `status` ENUM('ON', 'OFF') NULL,
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME NULL,
     PRIMARY KEY (`user_id`),
     CONSTRAINT `fk_user_profile_user`
         FOREIGN KEY (`user_id`)
@@ -122,6 +110,7 @@ CREATE TABLE IF NOT EXISTS `hillel_shop`.`user_profile` (
 CREATE TABLE IF NOT EXISTS `hillel_shop`.`order_product` (
     `order_id` INT UNSIGNED NOT NULL,
     `product_id` INT UNSIGNED NOT NULL,
+    `quantity` INT UNSIGNED NOT NULL,
     INDEX `fk_order_has_product_product_idx` (`product_id`),
     INDEX `fk_order_has_product_order_idx` (`order_id`),
     CONSTRAINT `fk_order_product_order`
